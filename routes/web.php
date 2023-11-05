2049e5508f52f1ac9540d693a6f569d7c7938c05
LHH2904 <102815563+LHH2904@users.noreply.github.com>
2023-11-06 00:44:12 +0700

    Update Profile Admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -28,4 +31,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+Route::get('admin/login', [AdminController::class, 'login'])->name('admin.login');
