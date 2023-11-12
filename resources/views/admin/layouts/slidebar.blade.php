77445a0e4227ad6b67ca0ae8cca68bbadbd232ba
LHH2904 <102815563+LHH2904@users.noreply.github.com>
2023-11-12 23:27:50 +0700

    CRUD brand of product

--- resources/views/admin/layouts/slidebar.blade.php
+++ resources/views/admin/layouts/slidebar.blade.php
@@ -13,7 +13,7 @@
                         class="fas fa-fire"></i><span>Dashboard</span></a>
             </li>
             <li class="menu-header">Starter</li>
-
+            {{-- Category --}}
             <li
                 class="dropdown {{ setActive(['admin.category.*', 'admin.sub-category.*', 'admin.child-category.*']) }}">
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
@@ -27,7 +27,16 @@
                             href="{{ route('admin.child-category.index') }}">Child Category</a></li>
                 </ul>
             </li>
-
+            {{-- Product --}}
+            <li class="dropdown {{ setActive(['admin.brand.*']) }}">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
+                    <span>Manage Product</span></a>
+                <ul class="dropdown-menu">
+                    <li class="{{ setActive(['admin.brand.*']) }}"><a class="nav-link"
+                            href="{{ route('admin.brand.index') }}">Brands</a></li>
+                </ul>
+            </li>
+            {{-- Slider --}}
             <li class="dropdown {{ setActive(['admin.slider.*']) }}">
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
                     <span>Manage Sliders</span></a>
